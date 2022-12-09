8855323194b083560500b9a8ddf1b4de3cf32402
DaddaFarouk <faroukdadda@gmail.com>
2022-12-09 18:38:01 +0100

    added crossOrigin

--- src/main/java/com/farouk/billingservice/web/BillingRestController.java
+++ src/main/java/com/farouk/billingservice/web/BillingRestController.java
@@ -6,11 +6,13 @@ import com.farouk.billingservice.feign.ProductItemRestClient;
 import com.farouk.billingservice.models.Customer;
 import com.farouk.billingservice.models.Product;
 import com.farouk.billingservice.repositories.BillRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class BillingRestController {
     private final BillRepository billRepository;
     private final CustomerRestClient customerRestClient;
